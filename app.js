b738d65c806bc6e014427a1fbc01f0abb21e4e0a
Nikita Lavrov <nlavrov2007@gmail.com>
2023-01-30 18:36:46 +0100

    Cookies allowed

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
