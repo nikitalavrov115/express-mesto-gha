8ac3616f3e36fe564ecb8e43ecccb29d9b8d9436
Nikita Lavrov <nlavrov2007@gmail.com>
2023-01-30 20:20:11 +0100

    sameSite added

--- controllers/users.js
+++ controllers/users.js
@@ -95,7 +95,7 @@ module.exports.login = (req, res, next) => {
       }
 
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 604800000, httpOnly: true });
+      res.cookie('jwt', token, { maxAge: 604800000, httpOnly: true, sameSite: true });
       res.status(200).send({ jwt: token });
     })
     .catch(next);
