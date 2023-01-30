5c906df581cbbc9364edfb9938430ba677c461dd
Nikita Lavrov <nlavrov2007@gmail.com>
2023-01-30 17:22:26 +0100

    CORS added

--- app.js
+++ app.js
@@ -26,6 +26,34 @@ const limiter = rateLimit({
   max: 100,
 });
 
+const allowedCors = [
+  'http://api.mesto.nikitalavrov.nomoredomainsclub.ru',
+  'https://api.mesto.nikitalavrov.nomoredomainsclub.ru/',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
+  }
+
+  next();
+  return null;
+});
+
 app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
